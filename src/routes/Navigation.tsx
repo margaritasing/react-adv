e3980c75e013478866e17963722e3e164ee64527
margaritasing <libiamfreitesm@gmail.com>
2022-12-15 19:51:25 -0300

    actualizo el react router dom a v6

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,9 +1,12 @@
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
   NavLink
 } from 'react-router-dom';
+import LazyPage1 from '../lazyload/Page/LazyPage1';
+import LazyPage2 from '../lazyload/Page/LazyPage2';
+import LazyPage3 from '../lazyload/Page/LazyPage3';
 
 import logo from '../logo.svg';
 
@@ -15,30 +18,27 @@ export const Navigation = () => {
             <img src={ logo } alt="React Logo" />
           <ul>
             <li>
-              <NavLink to="/" activeClassName="nav-active" exact>Home</NavLink>
+              <NavLink to="/lazy1" className={ ({ isActive   }) => isActive? 'nav-active': '' } >Home</NavLink>
             </li>
             <li>
-              <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
+              <NavLink to="/lazy2" className={ ({ isActive   }) => isActive? 'nav-active': '' } >About</NavLink>
             </li>
             <li>
-              <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
+              <NavLink to="/lazy3" className={ ({ isActive   }) => isActive? 'nav-active': '' } >Users</NavLink>
             </li>
           </ul>
         </nav>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <h1>About</h1>
-          </Route>
-          <Route path="/users">
-            <h1>Users</h1>
-          </Route>
-          <Route path="/">
-            <h1>Home</h1>
-          </Route>
-        </Switch>
+        
+          <Routes>
+          <Route path="/lazy1" element= {<LazyPage1/>}/>
+          <Route path="/lazy2" element= {<LazyPage2/>}/>
+          <Route path="/lazy3" element= {<LazyPage3/>}/>
+          <Route path="/*"><h1>Home</h1></Route>
+          </Routes>
+          
       </div>
     </Router>
   );
