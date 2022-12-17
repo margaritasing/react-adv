88494618bf33c68bd078b27425508b0d615793a7
margaritasing <libiamfreitesm@gmail.com>
2022-12-17 17:13:13 -0300

    subo nuevos componentes

--- src/lazyload/Layout/LazyLayout.tsx
+++ src/lazyload/Layout/LazyLayout.tsx
@@ -4,6 +4,8 @@ export const LazyLayout = () =>{
     return (
         <div>
             <h1>LazyLayout Page</h1>
+
+            {/* Rutas Hijas aqui */}
         </div>
     )
 }
---
+++ src/lazyload/Page/NoLazy.tsx
@@ -0,0 +1,12 @@
+
+
+
+const NoLazy = () => {
+  return (
+    <div>
+        <h1>NoLazy</h1>
+    </div>
+  )
+}
+
+export default NoLazy
